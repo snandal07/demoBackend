786b5daa5b168b144d12f2e4e2735feac2ec71df
Sahil Nandal <sahilnandalsahil@gmail.com>
2026-03-22 14:38:10 +0530

    demo data added

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+const demoData = {
+  id: "0001",
+  type: "donut",
+  name: "Cake",
+  ppu: 0.55,
+  batters: {
+    batter: [
+      { id: "1001", type: "Regular" },
+      { id: "1002", type: "Chocolate" },
+      { id: "1003", type: "Blueberry" },
+      { id: "1004", type: "Devil's Food" },
+    ],
+  },
+  topping: [
+    { id: "5001", type: "None" },
+    { id: "5002", type: "Glazed" },
+    { id: "5005", type: "Sugar" },
+    { id: "5007", type: "Powdered Sugar" },
+    { id: "5006", type: "Chocolate with Sprinkles" },
+    { id: "5003", type: "Chocolate" },
+    { id: "5004", type: "Maple" },
+  ],
+};
+
+app.get("/demo", (req, res) => {
+  res.json(demoData);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
